f8fcdb2a74aa3ec641e94d1616394db4749f238e
adamclement-exe <57185830+adamclement-exe@users.noreply.github.com>
2021-07-01 15:52:39 +0100

    Update database.js

--- database.js
+++ database.js
@@ -11,13 +11,19 @@ let db = new sqlite3.Database('./questions.db', (err) => {
 });
 
 */
-const mongoServer = require('mongodb');
-const mongoClient = mongoServer.MongoClient;
-const dbName = 'aaa';
-const serverUrl = 'mongodb+srv://aaa:uR2PckB4TvEvQxRW@cluster0.y12j6.mongodb.net/aaa?retryWrites=true&w=majority';
 
-// Create a database
-const dbUrl =  serverUrl + dbName;
-const client = new mongoClient(dbUrl, { useUnifiedTopology: true});
 
-module.exports = client;
+//Import the mongoose module
+var mongoose = require('mongoose');
+
+//Set up default mongoose connection
+var mongoDB = 'mongodb+srv://aaa:uR2PckB4TvEvQxRW@cluster0.y12j6.mongodb.net/aaa?retryWrites=true&w=majority';
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+
+//Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+module.exports = db;
