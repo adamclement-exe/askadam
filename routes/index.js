1be01768f93aaf5b980619655cb186798e0cd332
adamclement-exe <57185830+adamclement-exe@users.noreply.github.com>
2021-07-01 10:53:39 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+ res.redirect('/questions/question-list-user');
 });
 
 module.exports = router;
