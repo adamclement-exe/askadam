6a4bc87b4d0d7e03454f2e95c047e12717bca6c8
adamclement-exe <57185830+adamclement-exe@users.noreply.github.com>
2021-07-01 15:31:55 +0100

    Update database.js

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
 const express = require('express');
 const router = express.Router();
-
+/*
 // open database in memory
 let db = new sqlite3.Database('./questions.db', (err) => {
   if (err) {
@@ -11,3 +11,23 @@ let db = new sqlite3.Database('./questions.db', (err) => {
 });
 module.exports = db;
 
+*/
+const mongoServer = require('mongodb');
+const mongoClient = mongoServer.MongoClient;
+const dbName = 'aaa';
+const serverUrl = 'mongodb+srv://aaa:uR2PckB4TvEvQxRW@cluster0.y12j6.mongodb.net/aaa?retryWrites=true&w=majority';
+
+// Create a database
+const dbUrl =  serverUrl + dbName;
+const client = new mongoClient(dbUrl, { useUnifiedTopology: true});
+
+client.connect( (err, db) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    else {
+        console.log('Database successfully created!');
+        db.close();
+    }
+});
